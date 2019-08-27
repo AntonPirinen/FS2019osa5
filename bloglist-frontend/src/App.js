1bd2a9bcf4971c1a5705fe518f95cd61bc172b4d
Anton Pirinen <anton.pirinen@helsinki.fi>
2019-08-27 19:47:35 +0300

    5.1

---
+++ bloglist-frontend/src/App.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react';
+import blogService from './services/blogs'
+import loginService from './services/login'
+import Blog from './components/Blog' 
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]) 
+  const [newBlog, setNewBlog] = useState('')
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+  
+  useEffect( () => {
+    blogService
+      .getAll().then(initialBlogs => {
+        setBlogs(initialBlogs)
+        console.log(initialBlogs)
+      })
+  }, [])
+
+  /* useEffect( () => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, []) */
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  /* const addBlog = () => {
+    return console.log("ok")
+  }
+
+    const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <input
+        value={newBlog}
+        onChange={handleBlogChange}
+      />
+      <button type="submit">save</button>
+    </form>  
+  ) */
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {loginForm()}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {`${user.name} logged in`}
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App;
